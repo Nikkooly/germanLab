e00560b44489c041fe03177f3329bf83d4bccc7a
Nikkooly <acaroshen@gmail.com>
2020-03-02 10:48:44 +0300

    first commit

---
+++ lab8/app/src/main/java/bstu/fit/german/lab8/MainActivity.java
@@ -0,0 +1,105 @@
+package bstu.fit.german.lab8;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_READ_CONTACTS=1;
+    private static boolean READ_CONTACTS_GRANTED =false;
+    private static String NAME_CONTACT;
+EditText editText;
+    ListView contactList;
+    ArrayList<String> contacts = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+editText=findViewById(R.id.editText);
+        contactList = (ListView) findViewById(R.id.contactList);
+
+        // получаем разрешения
+        int hasReadContactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        // если устройство до API 23, устанавливаем разрешение
+        if(hasReadContactPermission == PackageManager.PERMISSION_GRANTED){
+            READ_CONTACTS_GRANTED = true;
+        }
+        else{
+            // вызываем диалоговое окно для установки разрешений
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS);
+        }
+        // если разрешение установлено, загружаем контакты
+        if (READ_CONTACTS_GRANTED){
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        switch (requestCode){
+            case REQUEST_CODE_READ_CONTACTS:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    READ_CONTACTS_GRANTED = true;
+                }
+        }
+        if(READ_CONTACTS_GRANTED){
+
+        }
+        else{
+            Toast.makeText(this, "Требуется установить разрешения", Toast.LENGTH_LONG).show();
+        }
+    }
+public void Load(View view){
+        loadContacts();
+}
+    private void loadContacts(){
+        NAME_CONTACT=editText.getText().toString();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if(cursor!=null){
+            while (cursor.moveToNext()) {
+
+                // получаем каждый контакт
+                String contact = cursor.getString(
+                        cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                // добавляем контакт в список
+                contacts.add(contact);
+            }
+            cursor.close();
+        }
+        if(contacts.contains(NAME_CONTACT)){
+            Toast.makeText(this, "Контакт найден", Toast.LENGTH_LONG).show();
+           Integer position= contacts.indexOf(NAME_CONTACT);
+           ArrayList<String> contact=new ArrayList<String>();
+           contact.add(contacts.get(position));
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, contact);
+            // устанавливаем для списка адаптер
+            contactList.setAdapter(adapter);
+        }
+        else{
+            Toast.makeText(this, "Совпадений не найдено", Toast.LENGTH_LONG).show();
+        }
+
+        // создаем адаптер
+
+    }
+}
