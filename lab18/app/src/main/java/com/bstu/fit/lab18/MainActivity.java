5bb49c5adb47efeea15679f370950b184a5d0b29
Nikkooly <acaroshen@gmail.com>
2020-05-19 14:27:08 +0300

    release

---
+++ lab18/app/src/main/java/com/bstu/fit/lab18/MainActivity.java
@@ -0,0 +1,139 @@
+package com.bstu.fit.lab18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
+    private Bitmap bitmap = null;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //загрузка рисунка
+                checkInternetConenction();
+                downloadImage("https://hb.bizmrg.com/ex-press/images/content/x1024x1024/c3a403eaf82be4ac51ed8c632c3089c5-f24d80acb4ee32776f2667ff8d6452cb2ca88fa8.jpeg");
+            }
+        });
+    }
+
+    private void downloadImage(String urlStr) {
+        progressDialog = ProgressDialog.show(this, "", "Downloading Image from " + urlStr);
+        final String url = urlStr;
+        new Thread() {
+            public void run() {
+                InputStream in = null;
+
+                Message msg = Message.obtain();
+                msg.what = 1;
+
+                try {
+                    in = openHttpConnection(url);
+                    bitmap = BitmapFactory.decodeStream(in);
+                    Bundle b = new Bundle();
+                    b.putParcelable("bitmap", bitmap);
+                    msg.setData(b);
+                    in.close();
+                }catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                messageHandler.sendMessage(msg);
+            }
+        }.start();
+    }
+
+    private InputStream openHttpConnection(String urlStr) {
+        InputStream in = null;
+        int resCode = -1;
+
+        try {
+            URL url = new URL(urlStr);
+            URLConnection urlConn = url.openConnection();
+
+            if (!(urlConn instanceof HttpURLConnection)) {
+                throw new IOException("URL is not an Http URL");
+            }
+
+            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            resCode = httpConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return in;
+    }
+
+    private Handler messageHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap((Bitmap)
+                    (msg.getData().getParcelable("bitmap")));
+            progressDialog.dismiss();
+        }
+    };
+
+    private boolean checkInternetConenction() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager connec
+                =(ConnectivityManager)getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+
+        // Check for network connections
+        if ( connec.getNetworkInfo(0).getState() ==
+                android.net.NetworkInfo.State.CONNECTED ||
+                connec.getNetworkInfo(0).getState() ==
+                        android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() ==
+                        android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ) {
+            Toast.makeText(this, " Connected ", Toast.LENGTH_LONG).show();
+            return true;
+        }else if (
+                connec.getNetworkInfo(0).getState() ==
+                        android.net.NetworkInfo.State.DISCONNECTED ||
+                        connec.getNetworkInfo(1).getState() ==
+                                android.net.NetworkInfo.State.DISCONNECTED  ) {
+            Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return false;
+    }
+
+}
+
